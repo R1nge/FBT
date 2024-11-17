8558298c9bcd217ef734c99566cb41cbb3f6e383
R1nge <r1ngegamedev@gmail.com>
2024-11-17 15:42:03 +0300

    server feat: camera input

--- Server/main.py
+++ Server/main.py
@@ -1,13 +1,8 @@
+import cv2
 import mediapipe as mp
 import numpy as np
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
-from mediapipe.framework.formats import landmark_pb2
-from mediapipe import solutions
-import cv2
 
 model_path = 'E:/UnityProjects/FBT/Server/pose_landmarker_full.task'
-
 BaseOptions = mp.tasks.BaseOptions
 PoseLandmarker = mp.tasks.vision.PoseLandmarker
 PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
@@ -15,41 +10,40 @@ PoseLandmarkerResult = mp.tasks.vision.PoseLandmarkerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
 
-def draw_landmarks_on_image(rgb_image, detection_result):
-  pose_landmarks_list = detection_result.pose_landmarks
-  annotated_image = np.copy(rgb_image)
-
-  # Loop through the detected poses to visualize.
-  for idx in range(len(pose_landmarks_list)):
-    pose_landmarks = pose_landmarks_list[idx]
-
-    # Draw the pose landmarks.
-    pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-    pose_landmarks_proto.landmark.extend([
-      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
-    ])
-    solutions.drawing_utils.draw_landmarks(
-      annotated_image,
-      pose_landmarks_proto,
-      solutions.pose.POSE_CONNECTIONS,
-      solutions.drawing_styles.get_default_pose_landmarks_style())
-  return annotated_image
-
-
-# Create a pose landmarker instance with the live stream mode:
 def print_result(result: PoseLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
     print('pose landmarker result: {}'.format(result))
 
+
 options = PoseLandmarkerOptions(
-    base_options=BaseOptions(model_asset_path=model_path)
-    #running_mode=VisionRunningMode.LIVE_STREAM,
-    #result_callback=print_result)
-)
+    base_options=BaseOptions(model_asset_path=model_path),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=print_result)
 
 with PoseLandmarker.create_from_options(options) as landmarker:
-    image = mp.Image.create_from_file("image.jpg")
-    detection_result = landmarker.detect(image)
-    annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
-    cv2.imshow('image', cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
-    cv2.waitKey(0)
-    print("a")
+
+    # Use OpenCV’s VideoCapture to start capturing from the webcam.
+    cap = cv2.VideoCapture(0)
+
+    while cap.isOpened():
+        success, frame = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+            continue
+
+        # Convert the frame received from OpenCV to a MediaPipe’s Image object.
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+
+        # Process the image and get the result
+        detection_result = landmarker.detect_async(mp_image, int(cv2.getTickCount() / cv2.getTickFrequency() * 1000))
+
+        # Draw landmarks on the image
+        #annotated_image = draw_landmarks_on_image(frame, detection_result)
+
+        # Display the annotated image
+        cv2.imshow('Annotated Image', frame)
+
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
